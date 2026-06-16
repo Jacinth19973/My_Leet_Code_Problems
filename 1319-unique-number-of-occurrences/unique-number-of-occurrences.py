b7e7e3d9d067358e7ec5c2143995cfa3302b2af0
Jacinth Manuel J <jacinthmanul2310_bai27@mepcoeng.ac.in>
2026-06-16 16:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (40.05%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def uniqueOccurrences(self, arr: list[int]) -> bool:
+        counts = Counter(arr).values()
+        return len(counts) == len(set(counts))
