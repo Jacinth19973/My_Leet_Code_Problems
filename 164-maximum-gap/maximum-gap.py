8c1b8a7923f8a9ef00340cc3adc7495faef80d80
Jacinth Manuel J <jacinthmanul2310_bai27@mepcoeng.ac.in>
2026-06-18 16:11:09 +0530

    Time: 142 ms (75.86%) | Memory: 31.9 MB (74.91%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        nums.sort()
+        if len(nums)<2:
+            return 0
+        max_diff=0
+        for i in range(len(nums)-1):
+            diff=nums[i+1]-nums[i]
+            if diff>max_diff:
+                max_diff=diff
+        return max_diff
+
+        
