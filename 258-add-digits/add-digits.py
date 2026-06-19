b177732cd837d4688d789b83995328cdfb66b353
Jacinth Manuel J <jacinthmanul2310_bai27@mepcoeng.ac.in>
2026-06-19 12:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (51.63%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while(num>9):
+            num=sum(int(digit)for digit in str(num))
+        return num
+            
+            
+        
